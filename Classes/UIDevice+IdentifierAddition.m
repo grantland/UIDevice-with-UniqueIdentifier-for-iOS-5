5915b35007016cb049fb46bd09e0e5b05c52e7a9
Grantland Chew <grantlandchew@gmail.com>
2012-03-26 17:32:50 -0700

    added FIX_CATEGORY_BUG to remove the need for -all_load

--- Classes/UIDevice+IdentifierAddition.m
+++ Classes/UIDevice+IdentifierAddition.m
@@ -8,13 +8,14 @@
 
 #import "UIDevice+IdentifierAddition.h"
 #import "NSString+MD5Addition.h"
+#import "CategoryFix.h"
 
 #include <sys/socket.h> // Per msqr
 #include <sys/sysctl.h>
 #include <net/if.h>
 #include <net/if_dl.h>
 
-@interface UIDevice(Private)
+@interface UIDevice (Private)
 
 - (NSString *) macaddress;
 
@@ -30,48 +31,48 @@
 // Courtesy of FreeBSD hackers email list
 // Accidentally munged during previous update. Fixed thanks to erica sadun & mlamb.
 - (NSString *) macaddress{
-    
+
     int                 mib[6];
     size_t              len;
     char                *buf;
     unsigned char       *ptr;
     struct if_msghdr    *ifm;
     struct sockaddr_dl  *sdl;
-    
+
     mib[0] = CTL_NET;
     mib[1] = AF_ROUTE;
     mib[2] = 0;
     mib[3] = AF_LINK;
     mib[4] = NET_RT_IFLIST;
-    
+
     if ((mib[5] = if_nametoindex("en0")) == 0) {
         printf("Error: if_nametoindex error\n");
         return NULL;
     }
-    
+
     if (sysctl(mib, 6, NULL, &len, NULL, 0) < 0) {
         printf("Error: sysctl, take 1\n");
         return NULL;
     }
-    
+
     if ((buf = malloc(len)) == NULL) {
         printf("Could not allocate memory. error!\n");
         return NULL;
     }
-    
+
     if (sysctl(mib, 6, buf, &len, NULL, 0) < 0) {
         printf("Error: sysctl, take 2");
         free(buf);
         return NULL;
     }
-    
+
     ifm = (struct if_msghdr *)buf;
     sdl = (struct sockaddr_dl *)(ifm + 1);
     ptr = (unsigned char *)LLADDR(sdl);
-    NSString *outstring = [NSString stringWithFormat:@"%02X:%02X:%02X:%02X:%02X:%02X", 
+    NSString *outstring = [NSString stringWithFormat:@"%02X:%02X:%02X:%02X:%02X:%02X",
                            *ptr, *(ptr+1), *(ptr+2), *(ptr+3), *(ptr+4), *(ptr+5)];
     free(buf);
-    
+
     return outstring;
 }
 
@@ -82,18 +83,19 @@
 - (NSString *) uniqueDeviceIdentifier{
     NSString *macaddress = [[UIDevice currentDevice] macaddress];
     NSString *bundleIdentifier = [[NSBundle mainBundle] bundleIdentifier];
-    
+
     NSString *stringToHash = [NSString stringWithFormat:@"%@%@",macaddress,bundleIdentifier];
     NSString *uniqueIdentifier = [stringToHash stringFromMD5];
-    
+
     return uniqueIdentifier;
 }
 
 - (NSString *) uniqueGlobalDeviceIdentifier{
     NSString *macaddress = [[UIDevice currentDevice] macaddress];
     NSString *uniqueIdentifier = [macaddress stringFromMD5];
-    
+
     return uniqueIdentifier;
 }
 
 @end
+FIX_CATEGORY_BUG(UIDevice)
